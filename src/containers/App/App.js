c0d4aa21eb28d4bdb2b84d0836404bf1ee9cd316
MarvinAmador7 <amador@18techs.com>
2016-11-27 01:36:17 -0600

    add translation binding

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -4,7 +4,8 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { fetchWelcomeMessage } from '../../actions/welcome';
-
+import { changeLang } from '../../actions/lang';
+import { Translate } from 'react-redux-i18n';
 import logo from '../../assets/logo.svg';
 import './App.css';
 
@@ -14,7 +15,7 @@ function mapStateToProps (state) {
 
 function mapDispatchToProps (dispatch) {
   return {
-    actions : bindActionCreators({ fetchWelcomeMessage }, dispatch),
+    actions : bindActionCreators({ fetchWelcomeMessage, changeLang }, dispatch),
   };
 }
 
@@ -30,16 +31,29 @@ class App extends Component {
     this.props.actions.fetchWelcomeMessage();
   }
 
+  handleLangChange = (event) => {
+    this.props.actions.changeLang(event.target.value);
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>{this.props.reducers.welcome.toJS().message}</h2>
+          <h2>
+            <Translate value="application.message"/>
+          </h2>
+
         </div>
         <p className="App-intro">
           {this.props.reducers.welcome.toJS().description}
         </p>
+        <br/>
+        <p>language : </p>
+        <select name="lang" id="" onChange={this.handleLangChange}>
+          <option value="en">EN</option>
+          <option value="es">ES</option>
+        </select>
       </div>
     );
   }
