61f6e4535fb472f870849f29bac453225dcf198d
MarvinAmador7 <amador@18techs.com>
2016-11-27 01:34:43 -0600

    add i18n translations file

---
+++ src/i18n/index.js
@@ -0,0 +1,22 @@
+const translations = {
+   en: {
+    application: {
+      message     : 'Welcome to the brand new 4tius web Application',
+      description : 'React - Redux - Webpack - Immutable - ES6 - i18n',
+    },
+    date: {
+      long: 'MMMM Do, YYYY'
+    }
+  },
+  es: {
+    application: {
+      message     : 'Bienvenidos a la nueva aplicacion web 4tius',
+      description : 'React - Redux - Webpack - Immutable - ES6 - i18n',
+    },
+    date: {
+      long: 'D MMMM YYYY'
+    }
+  }
+};
+
+export default translations;
